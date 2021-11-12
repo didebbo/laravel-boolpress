f674832daec4d9607c131873ad7281ca09a8784a
didebbo <gianluca.napoletano.93@gmail.com>
2021-11-12 16:55:43 +0100

    dashboard admin

--- app/Http/Controllers/Admin/HomeController.php
+++ app/Http/Controllers/Admin/HomeController.php
@@ -4,11 +4,12 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Post;
 
 class HomeController extends Controller
 {
     public function index()
     {
-        return view('admin.index');
+        return view('admin.index', ['posts' => Post::all()]);
     }
 }
--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -6,30 +6,20 @@
             <thead>
                 <tr>
                     <th scope="col">#</th>
-                    <th scope="col">First</th>
-                    <th scope="col">Last</th>
-                    <th scope="col">Handle</th>
+                    <th scope="col">Title</th>
+                    <th scope="col">Created</th>
+                    <th scope="col">Last Edit</th>
                 </tr>
             </thead>
             <tbody>
-                <tr>
-                    <th scope="row">1</th>
-                    <td>Mark</td>
-                    <td>Otto</td>
-                    <td>@mdo</td>
-                </tr>
-                <tr>
-                    <th scope="row">2</th>
-                    <td>Jacob</td>
-                    <td>Thornton</td>
-                    <td>@fat</td>
-                </tr>
-                <tr>
-                    <th scope="row">3</th>
-                    <td>Larry</td>
-                    <td>the Bird</td>
-                    <td>@twitter</td>
-                </tr>
+                @foreach ($posts as $post)
+                    <tr>
+                        <th scope="row">{{ $post['id'] }}</th>
+                        <td>{{ $post['title'] }}</td>
+                        <td>{{ $post['created_at'] }}</td>
+                        <td>{{ $post['updated_at'] }}</td>
+                    </tr>
+                @endforeach
             </tbody>
         </table>
     </div>
