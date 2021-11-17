ee306fec4cc061a48f03e176203b8a6671e033e8
didebbo <gianluca.napoletano.93@gmail.com>
2021-11-17 21:45:07 +0100

    refactoring

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,8 +1,8 @@
 <header class="blog-header py-3">
     <div class="row flex-nowrap justify-content-center align-items-center">
-        <div class="col-4 text-center">
+        <div class="col-2 text-center">
             <h1>
-                <a class="blog-header-logo text-dark" href="#">Boolpress</a>
+                <a class="blog-header-logo text-dark" href="{{ route('admin.index') }}">Boolpress</a>
             </h1>
         </div>
     </div>
